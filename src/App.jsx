0bb94b65cb2270635c80615a05321cb7af6cbc3e
Dhairya-Dave-962 <dhairyadave962@gmail.com>
2025-04-23 20:24:27 +0530

    Fixes to improve loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,31 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 import { Navbar, Footer, Loader } from "./components/component_index.js";
 import { Home, About, Contact, Projects } from "./pages/page_index.js";
 import "./App.css";
 
-function App() {
-  const [loading, setLoading] = useState(true);
+// Lazy load the three Spline components
+const SplineComputer = React.lazy(() =>
+  import("./components/SplineComputer/SplineComputer.jsx")
+);
+const SplineGlobe = React.lazy(() =>
+  import("./components/SplineGlobe/SplineGlobe.jsx")
+);
+const SplineBackground = React.lazy(() =>
+  import("./components/SplineBackground/SplineBackground.jsx")
+);
 
+function App() {
+  const [loading, setLoading] = useState(true); // State to control preloader visibility
+  const [isLoaded, setIsLoaded] = useState(false); // To track whether components are fully loaded
+
+  // Start loading the Spline components and other assets immediately
   useEffect(() => {
     setTimeout(() => {
       setLoading(false);
-    }, 3000);
+    }, 4000);
   }, []);
+
   return (
     <>
       {loading ? (
