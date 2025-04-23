b697b59a676bab1e0bc82f213fa4c52f77d8238e
Dhairya-Dave-962 <dhairyadave962@gmail.com>
2025-04-23 21:59:47 +0530

    Minor fix to improve perfomance

--- src/App.jsx
+++ src/App.jsx
@@ -4,41 +4,30 @@ import { Navbar, Footer, Loader } from "./components/component_index.js";
 import { Home, About, Contact, Projects } from "./pages/page_index.js";
 import "./App.css";
 
-// Lazy load the three Spline components
-const SplineComputer = React.lazy(() =>
-  import("./components/SplineComputer/SplineComputer.jsx")
-);
-const SplineGlobe = React.lazy(() =>
-  import("./components/SplineGlobe/SplineGlobe.jsx")
-);
-const SplineBackground = React.lazy(() =>
-  import("./components/SplineBackground/SplineBackground.jsx")
-);
-
 function App() {
-  const [loading, setLoading] = useState(true); // State to control preloader visibility
-  const [isLoaded, setIsLoaded] = useState(false); // To track whether components are fully loaded
+  const [isLoading, setIsLoading] = useState(true);
 
-  // Start loading the Spline components and other assets immediately
   useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 4000);
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
   }, []);
 
   return (
     <>
-      {loading ? (
+      {isLoading ? (
         <Loader />
       ) : (
         <>
           <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/projects" element={<Projects />} />
-          </Routes>
+          <Suspense fallback={<Loader />}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/projects" element={<Projects />} />
+            </Routes>
+          </Suspense>
           <Footer />
         </>
       )}
