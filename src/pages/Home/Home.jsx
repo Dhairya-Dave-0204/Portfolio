dd6d3815d0f79b782d0fec51605d5898e4f8fc79
Dhairya-Dave-962 <dhairyadave962@gmail.com>
2025-04-30 16:06:44 +0530

    Fixes for load balancing

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Hero, ProjectBento, SkillCards, ContactForm, ShortAbout } from "../../components/component_index";
 
 function Home() {
+
+  useEffect(() => {
+    const components = import.meta.glob('../components/SplineBackground/SplineBackground.jsx');
+    components['../components/SplineBackground/SplineBackground.jsx']?.();
+  }, []);
   return (
     <>
       <Hero />
